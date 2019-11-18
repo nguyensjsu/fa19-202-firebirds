11285da58091ae04f678f3ae0dffd75961eaa9f6
HarshadaJiv <55175861+HarshadaJiv@users.noreply.github.com>
2019-11-17 23:05:11 -0800

    correct alignments

--- Greenfoot/OrangeAlien.java
+++ Greenfoot/OrangeAlien.java
@@ -3,15 +3,36 @@ import greenfoot.*;
 
 public class OrangeAlien extends Aliens
 {
+    private int health;
+    private int score;
     
-	public OrangeAlien() {
-		
-		score = 20;
-		alienexplosiveFactory = new MetalFactory();
-	}
-	
+    public OrangeAlien() {
+    	score = 50;
+    	health = 100;
+    	alienexplosiveFactory = new MetalFactory();
+    }
     public void act() 
     {
        super.act();
+       if (getX() == 0)
+       {
+            getWorld().removeObject(this);
+            MyWorld.myWorld.removeObserver(this);
+            return;
+       }
+       
+       // alien has been hit by a weapon
+       Weapon weapon = (Weapon) this.getOneIntersectingObject(Weapon.class);
+       if (weapon != null)
+       {
+            health -= weapon.getDamage();
+       }
+       if (health == 0)
+       {
+           // update score by 10
+           getWorld().getObjects(Counter.class).get(0).addScore(score);
+           getWorld().removeObject(this);
+           return;
+       }
     }    
 }
