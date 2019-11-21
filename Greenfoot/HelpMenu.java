e5064a7acd1c16068e80b6655847fec58027ea75
Mayura Dhivya Nehruji <13078871+MayuraDhivyaNehruji@users.noreply.github.com>
2019-11-20 22:32:57 -0800

    helpmenu ui

--- Greenfoot/HelpMenu.java
+++ Greenfoot/HelpMenu.java
@@ -1,9 +1,13 @@
-import greenfoot.*; 
-public class HelpMenu extends World
-{
-
-    public HelpMenu()
-    {    
-        super(600, 300, 1); 
+    import greenfoot.*; 
+    public class HelpMenu extends World
+    {
+    
+        public HelpMenu()
+      {    
+          super(600, 300, 1);
+        GreenfootImage image = new GreenfootImage("HelpContent.png");
+        //image.scale(600,300);
+        setBackground(image);
+     
     }
 }
