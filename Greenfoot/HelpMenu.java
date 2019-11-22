55bbbc15221a8af60c2c980dc9a123043e74c459
HarshadaJiv <55175861+HarshadaJiv@users.noreply.github.com>
2019-11-21 22:44:11 -0800

    setting right image size for help

--- Greenfoot/HelpMenu.java
+++ Greenfoot/HelpMenu.java
@@ -1,12 +1,17 @@
+/* 
+ * Show Help Contents
+ * 
+ */    
+
     import greenfoot.*; 
     public class HelpMenu extends World
     {
     
         public HelpMenu()
       {    
-          super(600, 300, 1);
+        super(1100, 700,1);
         GreenfootImage image = new GreenfootImage("HelpContent.png");
-        //image.scale(600,300);
+        image.scale(1100,700);
         setBackground(image);
      
     }
