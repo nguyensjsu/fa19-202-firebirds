96d8ae4d86bf6bcf9b188fda3c939e722098b0af
Sheethal Mathew <sheethalsmathew@gmail.com>
2019-11-22 23:13:14 -0800

    Update Button.java

--- Greenfoot/Button.java
+++ Greenfoot/Button.java
@@ -1,6 +1,7 @@
    import greenfoot.*;  
 
-
+/*Parent Class of button click initiation.  Act is modified later on depending on what the object requires at the time of generation.
+*/
 public class Button extends Actor
 {
    
