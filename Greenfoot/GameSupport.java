316a028c87e40e2523830c29d7af0c3f73eaceec
Mayura Dhivya Nehruji <13078871+MayuraDhivyaNehruji@users.noreply.github.com>
2019-11-22 22:54:38 -0800

    added comments

--- Greenfoot/GameSupport.java
+++ Greenfoot/GameSupport.java
@@ -1,8 +1,7 @@
 import java.util.Random;
 import greenfoot.Greenfoot;
 
-
-
+/* Define getter Methods that return objects that are used in MyWorld class */
 
 public class GameSupport {
 
@@ -13,23 +12,24 @@ public class GameSupport {
 			counter++;
 		}
 	}
-	
+	/*method to get random color to generate alien*/
 	public static String getRandomAlien(){
 		String[] color = {"yellow", "red", "orange", "random"};
 		return color[Math.abs((getRandNumber(color.length - 1) + 1) % color.length)];
 	}
-	
+	/*getter method for random number */
 	public static int getRandNumber(int seed){
 		Random random = new Random();
 		return random.nextInt(seed);
 	}
-
+	/*getter method for angle*/
+	
 	public static int getAngle(int x, int y, int x2, int y2){
 		double xd = x2 - x;
 		double yd = y2 - y;
 		return (int)Math.toDegrees(Math.atan2(yd, xd));
 	}
-
+	/*getter method for Angle in Range*/
 	public static int getAngleInRange(int start, int end){
 		return Greenfoot.getRandomNumber(end-start)+start;
 	}
