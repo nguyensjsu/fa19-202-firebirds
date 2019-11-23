ab135c753d0652271eec00acfa5c527224e0a4da
Sheethal Mathew <sheethalsmathew@gmail.com>
2019-11-22 23:11:41 -0800

    Update WideBlast.java

--- Greenfoot/WideBlast.java
+++ Greenfoot/WideBlast.java
@@ -1,5 +1,9 @@
 import greenfoot.*;  
 
+
+/**Child class of Blast.
+*/
+
 public class WideBlast extends Blast
 {
    
@@ -12,12 +16,16 @@ public class WideBlast extends Blast
 		com = component;
 	}
 	
+/* extends removal of object hit from super class,Blast*/	
     public void act() 
     {
     	super.act();
     }
-
-	public void generateBlast(int x, int y) {
+    
+    
+    /*It will generate blast at the location given, UI is updated for wide blast features.
+    */
+     public void generateBlast(int x, int y) {
     	com.generateBlast(x, y);
     	
     	MyWorld.myWorld.addObject(this, x, y);
