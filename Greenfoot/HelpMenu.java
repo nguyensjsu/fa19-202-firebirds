da6ddccc57cc4f206eefc32556833318909585d1
HarshadaJiv <55175861+HarshadaJiv@users.noreply.github.com>
2019-11-22 23:27:01 -0800

    go back to menu on click

--- Greenfoot/HelpMenu.java
+++ Greenfoot/HelpMenu.java
@@ -17,4 +17,16 @@ public class HelpMenu extends World {
     setBackground(image);
 
   }
+  
+ /**
+   * Go back to menu otions on clicking screen
+   */
+  
+    public void act() {
+      if (Greenfoot.mouseClicked(this))  
+        {  
+            Greenfoot.setWorld(new MenuWorld());
+           
+        }  
+    }
 }
