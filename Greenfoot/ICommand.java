2a9621035c44f4939cf24adc834fad4322aa134b
Mayura Dhivya Nehruji <13078871+MayuraDhivyaNehruji@users.noreply.github.com>
2019-11-22 23:09:32 -0800

    added changes

--- Greenfoot/ICommand.java
+++ Greenfoot/ICommand.java
@@ -1,8 +1,8 @@
 import greenfoot.*;  
-
-
+/* interface for command pattern*/
 public interface ICommand
 {
+ /*method to set target receiver and execute actual command*/
  public void setReceiver(Receiver rcv);
     public void execute(); 
     
