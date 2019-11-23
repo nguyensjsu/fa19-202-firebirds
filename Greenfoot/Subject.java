7c26f019d54a73ff3953b0a77f6ae6b026f71d28
prathamr <prathamesh.karve@sjsu.edu>
2019-11-23 09:51:50 -0800

    Adding comments for subject interface

--- Greenfoot/Subject.java
+++ Greenfoot/Subject.java
@@ -1,10 +1,19 @@
 
 public interface Subject {
-
 	
-	
+	/*
+	 * Send notification to observers 
+	 */
 	public void notifyObserver(int X1, int X2);
+
+	/*
+	 * Add new observer
+	 */
 	public void addObserver(Observer sub);
+
+	/*
+	 * Remove an observer 
+	 */
 	public void removeObserver(Observer sub);
 	
 }
