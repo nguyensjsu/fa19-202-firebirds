a645f0a026ff51c9fa874c2848d08b20cb1e59a6
Mayura Dhivya Nehruji <13078871+MayuraDhivyaNehruji@users.noreply.github.com>
2019-11-22 23:00:27 -0800

    added comments

--- Greenfoot/Help.java
+++ Greenfoot/Help.java
@@ -1,13 +1,14 @@
 import greenfoot.*;  
-
+/* Concrete Command Class - Command Pattern*/
 public class Help implements ICommand
 {
     
     Receiver rcv;
-    
+    /* define execute method*/
      public void execute(){
     rcv.help();
     }
+    /*method to setReceiver*/
     public void setReceiver(Receiver rcv){
     this.rcv=rcv;
     }
