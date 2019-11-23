b1e81bf8c6cceddfaaecb7f37074f6a96ea6b8d3
Mayura Dhivya Nehruji <13078871+MayuraDhivyaNehruji@users.noreply.github.com>
2019-11-22 23:03:35 -0800

    added comments

--- Greenfoot/IAliens.java
+++ Greenfoot/IAliens.java
@@ -1,6 +1,6 @@
-
+/* define Alien Objects*/
 public class IAliens {
-
+	/* return different Alien Objects based on the condition* to generate random aliens*/
 	public static Aliens getAliens(String type){
 		
 		if(type.equalsIgnoreCase("orange")){
